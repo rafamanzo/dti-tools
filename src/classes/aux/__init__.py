3eb765a5efe1e416228e9b990d037344504ec3f4
Rafael Reggiain Manzo <rr.manzo@gmail.com>
2013-11-17 12:09:33 -0200

    DBSCAN initial implementation

---
+++ src/classes/aux/__init__.py
@@ -0,0 +1,1 @@
+"""Container for auxiliary algorithms implementations"""
---
+++ src/classes/aux/dbscan.py
@@ -0,0 +1,23 @@
+# This file is part of Improving Tractogrophy
+# Copyright (C) 2013 it's respectives authors (please see the AUTHORS file)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Container for DBSCAN class"""
+
+class DBSCAN(object):
+    "Implementation of the DBSCAN clustering algorithm"
+    def __init__(self, eps, min_pts):
+        self.eps = eps
+        self.min_pts = min_pts
