ca35f402501b1f9d19223a0463c81f7c6b1ac9fe
Rafael Reggiain Manzo <rr.manzo@gmail.com>
2013-11-17 17:44:59 -0200

    DBSCAN implemented (probably with errors)

--- src/classes/aux/dbscan.py
+++ src/classes/aux/dbscan.py
@@ -16,8 +16,58 @@
 
 """Container for DBSCAN class"""
 
+import numpy as np
+from sets import Set
+
 class DBSCAN(object):
-    "Implementation of the DBSCAN clustering algorithm"
-    def __init__(self, eps, min_pts):
-        self.eps = eps
-        self.min_pts = min_pts
+    """Implementation of the DBSCAN clustering algorithm"""
+    def __init__(self, eps, min_pts, mask, shape):
+        self.__eps = eps
+        self.__min_pts = min_pts
+        self.__mask = mask
+        self.__shape = shape
+        self.__result = np.zeros(self.shape,    # pylint: disable-msg=E1101
+                                 dtype=np.int16)# pylint: disable-msg=E1101
+
+    def __neighbourhood(self, point):
+        neighbourhood = {point}
+
+        for x in range(max((point[0] - self.eps,0)),
+                       min(self.__shape[0], point[0] + self.__eps + 1)):
+            for y in range(max((point[1] - self.eps,0)),
+                           min(self.__shape[1], point[1] + self.__eps + 1)):
+                for z in range(max((point[2] - self.eps,0)),
+                               min(self.__shape[2], point[2] + self.__eps + 1)):
+                        if self.__mask[x][y][z]:
+                            neighbourhood.add((x, y, z))
+
+        return neighbourhood
+
+    def __expand_cluster(self, point, neighbourhood, cluster):
+        for neighbour in neighbourhood:
+            if self.__result[neighbour[0]][neighbour[1]][neighbour[2]] == 0:
+                self.__result[neighbour[0]][neighbour[1]][neighbour[2]] = 1
+                cluster.add(neighbour)
+                neighbour_neighbourhood = self.__neighbourhood(neighbour)
+                if len(neighbour_neighbourhood) >= self.__min_pts:
+                    neighbourhood.update(neighbour_neighbourhood)
+                    self.__expand_cluster(point, neighbourhood, cluster)
+                    break
+
+    def fit():
+        clusters = set()
+        for x in range(x_range[0], x_range[1]):         # pylint: disable-msg=C0103,C0301
+            for y in range(y_range[0], y_range[1]):     # pylint: disable-msg=C0103,C0301
+                for z in range(z_range[0], z_range[1]): # pylint: disable-msg=C0103,C0301
+                    if self.__result[x][y][z] == 0:
+                        neighbourhood = self.__neighbourhood((x, y, z))
+                        if len(neighbourhood) < self.__min_pts
+                            self.__result[x][y][z] = -1
+                        else:
+                            cluster = set()
+                            self.__expand_cluster((x, y, z),
+                                                  neighbourhood,
+                                                  cluster)
+                            clusters.add(cluster)
+
+        return clusters, self.__result
