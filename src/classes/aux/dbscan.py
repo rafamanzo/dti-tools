4a2545e4726de378a9cf3613acc8eadfed592619
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-11-18 12:38:08 -0200

    Class DBSCAN fixed according to pylint (still misses tests)

--- src/classes/aux/dbscan.py
+++ src/classes/aux/dbscan.py
@@ -17,11 +17,14 @@
 """Container for DBSCAN class"""
 
 import numpy as np
-from sets import Set
+
+# pylint: disable-msg=R0903,R0922
 
 class DBSCAN(object):
     """Implementation of the DBSCAN clustering algorithm"""
+
     def __init__(self, eps, min_pts, mask, shape):
+        super(DBSCAN, self).__init__()
         self.__eps = eps
         self.__min_pts = min_pts
         self.__mask = mask
@@ -30,20 +33,22 @@ class DBSCAN(object):
                                  dtype=np.int16)# pylint: disable-msg=E1101
 
     def __neighbourhood(self, point):
+        """Receive a point and returns a set with it's neighbourhood"""
         neighbourhood = {point}
 
-        for x in range(max((point[0] - self.eps,0)),
+        for x in range(max((point[0] - self.__eps, 0)),                       # pylint: disable-msg=C0103,C0301
                        min(self.__shape[0], point[0] + self.__eps + 1)):
-            for y in range(max((point[1] - self.eps,0)),
+            for y in range(max((point[1] - self.__eps, 0)),                   # pylint: disable-msg=C0103,C0301
                            min(self.__shape[1], point[1] + self.__eps + 1)):
-                for z in range(max((point[2] - self.eps,0)),
+                for z in range(max((point[2] - self.__eps, 0)),               # pylint: disable-msg=C0103,C0301
                                min(self.__shape[2], point[2] + self.__eps + 1)):
-                        if self.__mask[x][y][z]:
-                            neighbourhood.add((x, y, z))
+                    if self.__mask[x][y][z]:
+                        neighbourhood.add((x, y, z))
 
         return neighbourhood
 
     def __expand_cluster(self, point, neighbourhood, cluster):
+        """Creates a cluster based on a given point and it's neighbourhood"""
         for neighbour in neighbourhood:
             if self.__result[neighbour[0]][neighbour[1]][neighbour[2]] == 0:
                 self.__result[neighbour[0]][neighbour[1]][neighbour[2]] = 1
@@ -54,14 +59,15 @@ class DBSCAN(object):
                     self.__expand_cluster(point, neighbourhood, cluster)
                     break
 
-    def fit():
+    def fit(self):
+        """For the given data, returns the clusters and result matrix"""
         clusters = set()
-        for x in range(x_range[0], x_range[1]):         # pylint: disable-msg=C0103,C0301
-            for y in range(y_range[0], y_range[1]):     # pylint: disable-msg=C0103,C0301
-                for z in range(z_range[0], z_range[1]): # pylint: disable-msg=C0103,C0301
+        for x in range(0, self.__shape[0]):         # pylint: disable-msg=C0103,C0301
+            for y in range(0, self.__shape[1]):     # pylint: disable-msg=C0103,C0301
+                for z in range(0, self.__shape[2]): # pylint: disable-msg=C0103,C0301
                     if self.__result[x][y][z] == 0:
                         neighbourhood = self.__neighbourhood((x, y, z))
-                        if len(neighbourhood) < self.__min_pts
+                        if len(neighbourhood) < self.__min_pts:
                             self.__result[x][y][z] = -1
                         else:
                             cluster = set()
