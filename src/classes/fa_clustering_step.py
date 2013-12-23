b15646189520fb624ebf30f1f17339f729cf0561
Rafael Reggiain Manzo <rr.manzo@gmail.com>
2013-12-23 17:39:43 -0200

    Just one mask file for FA clusters

--- src/classes/fa_clustering_step.py
+++ src/classes/fa_clustering_step.py
@@ -44,7 +44,6 @@ class FAClusteringStep(Step):
         self.maximum_fa_difference = -1.0
         self.tensor_data = [[[]]]
         self.affine = np.eye(4) # pylint: disable-msg=E1101
-        self.filtered_mask = []
 
     def validate_args(self):
         if len(sys.argv) != 6:
@@ -77,31 +76,25 @@ class FAClusteringStep(Step):
     def process(self):
         dbs = FADBSCAN(self.eps, self.min_pts, self.mask_data, self.shape,
                        self.tensor_data, self.maximum_fa_difference)
-        self.clusters, self.filtered_mask = dbs.fit()
+        self.clusters, _ = dbs.fit()
 
     def save(self):
-        cluster_number = 0
-        for cluster in self.clusters:
             cluster_img = nib.Nifti1Image(
-                                    self.__convert_cluster_to_mask(cluster),
+                                    self.__convert_clusters_to_mask(),
                                     self.affine)
-            cluster_img.to_filename('fa_cluster_'+str(cluster_number)+'_'+
+            cluster_img.to_filename('fa_clustered_'+
                                           sys.argv[1].split('/')[-1])
+
+    def __convert_clusters_to_mask(self):
+        """Gets clusters from DBSCAN and converts them into a mask"""
+
+        mask = np.zeros(self.shape, dtype=np.uint64) # pylint: disable-msg=E1101
+
+        cluster_number = 1
+        for cluster in self.clusters:
+            for point in cluster:
+                mask[point] = cluster_number
             cluster_number += 1
 
-        filtered_mask_img = nib.Nifti1Image(
-                                    self.filtered_mask,
-                                    self.affine)
-        filtered_mask_img.to_filename('fa_cluster_'+
-                                      'aggregated_'+
-                                      sys.argv[1].split('/')[-1])
-
-    def __convert_cluster_to_mask(self, cluster):
-        """Gets one cluster from DBSCAN and converts it into a mask"""
-
-        mask = np.zeros(self.shape, dtype=np.uint8) # pylint: disable-msg=E1101
-
-        for point in cluster:
-            mask[point] = 1
-
         return mask
+
