1ee4c881b82e6cd90e5592ac035c5471adf48223
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-04-03 22:21:08 -0300

    Fixed crop mask bugs

--- src/classes/crop_mask_step.py
+++ src/classes/crop_mask_step.py
@@ -21,10 +21,11 @@ import nibabel as nib           # Lib for reading and writing Nifit1
 import numpy as np              # Nibabel is based on Numpy
 
 from src.classes.base.cpu_parallel_step import CPUParallelStep
-from src.classes.aux.input_validators import validate_tensor_and_mask
+from src.classes.aux.input_validators import validate_mask
 
 class CropMaskStep(CPUParallelStep):
     def __init__(self):
+        super(CropMaskStep, self).__init__()
         self.mask = np.zeros((0, 0, 0))      # pylint: disable=E1101
         self.shape = (0, 0, 0)
         self.croped_mask = [[[]]]
@@ -50,12 +51,12 @@ class CropMaskStep(CPUParallelStep):
                       self.mask.shape[2])
         self.croped_mask = np.zeros(self.shape, np.uint32)
 
-        self.__x_start = sys.argv[2]
-        self.__x_end = sys.argv[3]
-        self.__y_start = sys.argv[4]
-        self.__y_end = sys.argv[5]
-        self.__z_start = sys.argv[6]
-        self.__z_end = sys.argv[7]
+        self.__x_start = int(sys.argv[2])
+        self.__x_end = int(sys.argv[3])
+        self.__y_start = int(sys.argv[4])
+        self.__y_end = int(sys.argv[5])
+        self.__z_start = int(sys.argv[6])
+        self.__z_end = int(sys.argv[7])
 
     def process_partition(self, x_range, y_range, z_range):
         for x in range(x_range[0], x_range[1]):         # pylint: disable=C0103,C0301
@@ -65,14 +66,14 @@ class CropMaskStep(CPUParallelStep):
                         (x >= self.__x_start and x <= self.__x_end) and
                         (y >= self.__y_start and y <= self.__y_end) and
                         (z >= self.__z_start and z <= self.__z_end)):
-                        self.queue.put((x,y,z))
+                        self.queue.put((x, y, z))
 
     def consume_product(self, product):
-        self.croped_mask[product] = self.mask[product]
+        self.croped_mask[product] = self.mask.get_data()[product]
 
     def save(self):
-        file_prefix = sys.argv[2].split('/')[-1].split('.')[0]
+        file_prefix = sys.argv[1].split('/')[-1].split('.')[0]
         croped_img = nib.Nifti1Image(
                             self.croped_mask, # pylint: disable=E1101
                             self.mask.get_affine())    # pylint: disable=E1101
-        croped_img.to_filename('%s_croped.nii.gz'%self.file_prefix)
+        croped_img.to_filename('%s_croped.nii.gz'%file_prefix)
