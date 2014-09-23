23bdd0e823e65f27238c9f704e87d273808eb62b
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-09-23 15:23:37 -0300

    Parallel SHS classification

--- tools/shs_classification/shs_mapper.py
+++ tools/shs_classification/shs_mapper.py
@@ -1,9 +1,25 @@
 import numpy as np
 import nibabel as nib
 
+from multiprocessing import Process, Queue, cpu_count
+from threading import Thread
+
 from tools.base import Base
 from tools.shs_classification.classifier import Classifier
 from tools.shs_classification.acquisition_direction import AcquisitionDirection
+
+class PoisonPill(object):
+    """PoisonPill stops a running parallel process"""
+    def __init__(self):
+        super(PoisonPill, self).__init__()
+
+class CoordinateClassification(object):
+    """CoordinateClassification pair"""
+    def __init__(self, coordinate, classification):
+        super(CoordinateClassification, self).__init__()
+        self.coordinate = coordinate
+        self.classification = classification
+
 
 class SHSMapper(Base):
     """Maps the whole dataset using the classifier"""
@@ -12,16 +28,19 @@ class SHSMapper(Base):
         self.__classifier = Classifier(float(significance_level), self.__load_acquisition_directions(acquisition_directions_path))
         self.__classification = np.zeros(self.mask.shape(), dtype=np.uint8)
 
-    def classify(self):
-        for i in range(self.mask.shape()[0]):
-            for j in range(self.mask.shape()[1]):
-                for k in range(self.mask.shape()[2]):
-                    if self.mask.inside((i,j,k)):
-                        print((i,j,k))
-                        self.__classification[(i,j,k)] = self.__classifier.classify(self.tensor_data.get((i,j,k))) + 1
+        self.__to_be_classified = Queue()
+        self.__classified = Queue()
+        self.__worker_count = cpu_count()
+
 
     def save(self):
         nib.Nifti1Image(self.__classification, self.mask.affine()).to_filename("%s" % self.output_path)
+
+    def run(self):
+        if self.__worker_count > 1:
+            self.__parallel_run()
+        else:
+            self.__sequential_run()
 
     def __load_acquisition_directions(self, acquisition_directions_path):
         acquisition_directions_file = open(acquisition_directions_path, 'r')
@@ -34,3 +53,62 @@ class SHSMapper(Base):
 
         return acquisition_directions
 
+    def __parallel_run(self):
+        classifiers = []
+
+        producer = Thread(target=self.__produce)
+        producer.start()
+
+        for _ in range(self.__worker_count):
+            classifiers.append(Process(target=self.__classify))
+            classifiers[-1].start()
+
+        consumer = Thread(target=self.__consume)
+        consumer.start()
+
+        producer.join()
+
+        for classifier in classifiers:
+            classifier.join()
+
+        self.__classified.put(PoisonPill())
+
+        consumer.join()
+
+    def __sequential_run(self):
+        for i in range(self.mask.shape()[0]):
+            for j in range(self.mask.shape()[1]):
+                for k in range(self.mask.shape()[2]):
+                    if self.mask.inside((i,j,k)):
+                        print((i,j,k))
+                        self.__classification[(i,j,k)] = self.__classifier.classify(self.tensor_data.get((i,j,k))) + 1
+
+    def __produce(self):
+        for i in range(self.mask.shape()[0]):
+            for j in range(self.mask.shape()[1]):
+                for k in range(self.mask.shape()[2]):
+                    if self.mask.inside((i,j,k)):
+                        self.__to_be_classified.put((i,j,k))
+
+        for _ in range(self.__worker_count):
+            self.__to_be_classified.put(PoisonPill())
+
+        return True
+
+    def __classify(self):
+        coordinate = self.__to_be_classified.get()
+
+        while not type(coordinate) is PoisonPill:
+            print(coordinate)
+            self.__classified.put(CoordinateClassification(coordinate, self.__classifier.classify(self.tensor_data.get(coordinate)) + 1))
+            coordinate = self.__to_be_classified.get()
+
+        return True
+
+    def __consume(self):
+        coordinate_classification = self.__classified.get()
+
+        while not type(coordinate_classification) is PoisonPill:
+            self.__classification[coordinate_classification.coordinate] = coordinate_classification.classification
+
+        return True
